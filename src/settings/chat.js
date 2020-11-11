ea7a527e3e143cbb29b275fa262aad57b69a4654
pedrohti <pedroh.ti@hotmail.com>
2020-11-11 20:57:33 -0300

    criado .env-example

--- src/settings/chat.js
+++ src/settings/chat.js
@@ -1,7 +1,7 @@
 const tmi = require("tmi.js");
 require("dotenv").config();
 
-const CHANNELS = ["gaules"];
+const CHANNELS = ["stickman1"];
 
 const client = new tmi.Client({
 	options: { debug: true },
@@ -11,15 +11,25 @@ const client = new tmi.Client({
 	},
 	identity: {
 		username: process.env.TWITCH_BOT_NAME,
-		password: process.env.TWITCH_TMI_TOKEN,
+		password: process.env.TWITCH_TMI_TOKEN_BOT,
 	},
 	channels: CHANNELS,
 });
 
 client.connect().catch(console.error);
 
+client.on("join", (channel, tags, message, self) => {
+	setInterval(() => {
+		var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+		client.say(
+			channel,
+			`/me ${randomColor} Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Si u mundo tá muito paradis?`,
+		);
+	}, 5000);
+});
+
 client.on("message", (channel, tags, message, self) => {
-	if (self) return;
+	// if (self) return;
 	if (message.toLowerCase() === "!hello") {
 		client.say(channel, `@${tags.username}, heya!`);
 	}
